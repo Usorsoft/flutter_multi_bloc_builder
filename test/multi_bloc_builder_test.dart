10f4f6022639090502a12685db5edf21e0570e67
Usor-Software <usor.info@gamil.com>
2020-03-05 22:25:46 +0100

    Add tests

--- test/multi_bloc_builder_test.dart
+++ test/multi_bloc_builder_test.dart
@@ -1,11 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_multi_bloc_builder/flutter_multi_bloc_builder.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'test_blocs.dart';
+
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
-    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+
+  testWidgets('Provide multiple states and update widget on state changes', (WidgetTester tester) async {
+    const BOTH_SLEEPING = "Both are slepping.";
+    const BOTH_RUNNING = "Both are runing.";
+    const DO_DIFFERENT_THINGS = "Person and dog do different things.";
+
+    final dogBloc = DogBloc();
+    final personBloc = PersonBloc();
+
+    final multiBlocBuilder = MultiBlocBuilder(
+      blocs: [dogBloc, personBloc], 
+      builder: (context, states) {
+        final dogState = states.get<DogState>();
+        final personState = states.get<PersonState>();
+
+        if (dogState is DogStateSleeping && personState is PersonStateSleeping) {
+          return Text(BOTH_SLEEPING);
+        }
+
+        if (dogState is DogStateRunning && personState is PersonStateRunning) {
+          return Text(BOTH_RUNNING);
+        }
+
+        return Text(DO_DIFFERENT_THINGS);
+      }
+    );
+
+    final material = MaterialApp(
+      home: multiBlocBuilder,
+    );
+
+    await tester.pumpWidget(material);
+    
+    expect(find.text(BOTH_SLEEPING), findsOneWidget);
+    expect(find.text(DO_DIFFERENT_THINGS), findsNothing);
+    expect(find.text(BOTH_RUNNING), findsNothing);
+
+    dogBloc.add(DogEventRun());
+
+    await tester.pump(const Duration(milliseconds: 1));
+    expect(find.text(DO_DIFFERENT_THINGS), findsOneWidget);
+    expect(find.text(BOTH_RUNNING), findsNothing);
+    expect(find.text(BOTH_SLEEPING), findsNothing);
+
+    personBloc.add(PersonEventWakeUp());
+
+    await tester.pump(const Duration(milliseconds: 1));
+    expect(find.text(DO_DIFFERENT_THINGS), findsOneWidget);
+    expect(find.text(BOTH_RUNNING), findsNothing);
+    expect(find.text(BOTH_SLEEPING), findsNothing);
+
+    personBloc.add(PersonEventRun());
+
+    await tester.pump(const Duration(milliseconds: 1));
+    expect(find.text(BOTH_RUNNING), findsOneWidget);
+    expect(find.text(DO_DIFFERENT_THINGS), findsNothing);
+    expect(find.text(BOTH_SLEEPING), findsNothing);
+
+    personBloc.add(PersonEventGoToBed());
+    dogBloc.add(DogEventGoToBed());
+
+    await tester.pump(const Duration(milliseconds: 1));
+    expect(find.text(BOTH_SLEEPING), findsOneWidget);
+    expect(find.text(BOTH_RUNNING), findsNothing);
+    expect(find.text(DO_DIFFERENT_THINGS), findsNothing);
+
+    dogBloc.close();
+    personBloc.close();
   });
 }
---
+++ test/test_blocs.dart
@@ -0,0 +1,64 @@
+
+import 'package:bloc/bloc.dart';
+
+abstract class PersonState {}
+class PersonStateRunning extends PersonState {}
+class PersonStateSleeping extends PersonState {}
+class PersonStateAwake extends PersonState {}
+
+abstract class PersonEvent {}
+class PersonEventRun extends PersonEvent {}
+class PersonEventGoToBed extends PersonEvent {}
+class PersonEventWakeUp extends PersonEvent {}
+
+class PersonBloc extends Bloc<PersonEvent, PersonState> {
+  @override
+  PersonState get initialState => PersonStateSleeping();
+
+  @override
+  Stream<PersonState> mapEventToState(PersonEvent event) async* {
+    
+    if (event is PersonEventWakeUp) {
+      yield PersonStateAwake();
+    }
+
+    else if (event is PersonEventRun) {
+      yield PersonStateRunning();
+    }
+
+    else if (event is PersonEventGoToBed) {
+      yield PersonStateSleeping();
+    }
+  }
+}
+
+abstract class DogState {}
+class DogStateBarking extends DogState {}
+class DogStateSleeping extends DogState {}
+class DogStateRunning extends DogState {}
+
+abstract class DogEvent {}
+class DogEventBark extends DogEvent {}
+class DogEventGoToBed extends DogEvent {}
+class DogEventRun extends DogEvent {}
+
+class DogBloc extends Bloc<DogEvent, DogState> {
+  @override
+  DogState get initialState => DogStateSleeping();
+
+  @override
+  Stream<DogState> mapEventToState(DogEvent event) async* {
+    
+    if (event is DogEventBark) {
+      yield DogStateBarking();
+    }
+
+    else if (event is DogEventGoToBed) {
+      yield DogStateSleeping();
+    }
+
+    else if (event is DogEventRun) {
+      yield DogStateRunning();
+    }
+  }
+}
